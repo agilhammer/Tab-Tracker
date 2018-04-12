cbceab5918efe752604b18107cce259c6785f42e
agilhammer <alejandro_gil_hammer@yahoo.com>
2018-04-12 17:07:42 -0500

    Create registration page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/pages/HelloWorld'
+import Register from '@/components/pages/Register'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
