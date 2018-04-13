8083b22ede8b86ad8d8289570175461da2029dba
agilhammer <alejandro_gil_hammer@yahoo.com>
2018-04-13 11:53:16 -0500

    Se agrega funcionalidad de registro

--- client/src/components/pages/Register.vue
+++ client/src/components/pages/Register.vue
@@ -1,12 +1,36 @@
 <template>
-   <div>
-     <h1>Register</h1>
-     <input type="email" name="email" placeholder="email" v-model="email"/>
-     <br>
-     <input type="password" name="password" placeholder="password" v-model="password"/>
-     <br>
-     <button @click="register">Register</button>
-   </div>
+  <v-layout>
+    <!-- <v-flex xs6 offset-xs3> -->
+     <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            v-model="email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            v-model="password" />
+          <br><br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -15,20 +39,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error {
+  color: red
+}
 </style>
